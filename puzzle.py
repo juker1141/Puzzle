1aa264517cf61275b1edfe02d03cfbea94ac57bd
Juker1141 <juker1141@gmail.com>
2019-05-02 21:34:43 +0800

    Update something

--- puzzle.py
+++ puzzle.py
@@ -1,7 +1,7 @@
 print('拆炸彈')
 import random
 puzzle = random.randint(1,10)
-
+count = 0
 while True:
 	pwd = input('請猜出1-100中的隨機整數: ')
 	pwd = int(pwd)
@@ -12,3 +12,5 @@ while True:
 		print('比' ,pwd ,'還要小')
 	elif pwd < puzzle:
 		print('比' ,pwd ,'還要大')
+	count = count + 1
+	print('已經試了', count, '次')
