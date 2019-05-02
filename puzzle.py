1bf56004e0b3f3a24694362b3f7957613a74408a
Juker1141 <juker1141@gmail.com>
2019-05-02 23:30:07 +0800

    Update something

--- puzzle.py
+++ puzzle.py
@@ -2,19 +2,20 @@ print('拆炸彈')
 import random
 start = int(input('請設定隨機整數最小值: '))
 end = int(input('請設定隨機整數最大值: '))
+message = '請猜出' + str(start) + '-' + str(end) + '中的隨機整數: '
 
 puzzle = random.randint(start, end)
 count = 0
 while True:
-	pwd = input('請猜出範圍中的隨機整數: ')
+	pwd = input(message)
 	pwd = int(pwd)
 	count = count + 1
 	if pwd == puzzle:
 		print('恭喜猜對了!')
-		print('已經試了', count, '次')
+		print('總共試了', count, '次')
 		break
 	elif pwd > puzzle:
 		print('比' ,pwd ,'還要小')
 	elif pwd < puzzle:
 		print('比' ,pwd ,'還要大')
-	print('已經試了', count, '次')
+	print('總共試了', count, '次')
